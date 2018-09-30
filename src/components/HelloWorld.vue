fecf2be12eee923b6b8be029d8b11a9a37be94bc
zrq1989 <41989818+zrq1989@users.noreply.github.com>
2018-09-30 20:31:30 +0800

    版本1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hello">
+        <button type="button" @click="changeClick">change</button>
+        sss
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        methods: {
+            changeClick() {
+                this.$http.get('./index.php').then((res) => console.log(res.data)).catch((res) => console.log(res))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+
+</style>
